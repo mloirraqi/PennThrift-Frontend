06eea5a750c11223d1e5c2896866c06202718069
mloirraqi <mloirraqi@gmail.com>
2022-04-02 17:53:12 -0400

    Implemented login & registering
    Implemented front-end of login and registration w/ error messages handled.

---
+++ src/pages/Login.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Form from '../components/Form';
+
+const Login = () =>{
+    
+    const [error, setError] = useState();
+    
+    function userDetails(username,password){
+        setError('We don’t recognize that username and password. Please try again.')
+    }
+
+    function reset(){
+        setError(null)
+    }
+
+    return(
+        <div className='grid grid-main justify-center w-full h-full items-center'>
+            <div className='col-span-8 flex flex-col justify-center'>
+                <div className='my-5 text-center text-5xl'>Welcome back!</div>
+                <div className='w-full my-10 h-[1px] bg-[gray]'></div>
+                <Form
+                    userDetails={userDetails}
+                    reset={reset}
+                    error={error}
+                    name='Login'/>
+            </div>
+        </div>
+        
+    )
+}
+
+
+export default Login;
---
+++ src/pages/Welcome.js
@@ -0,0 +1,40 @@
+import {  Link } from 'react-router-dom';
+
+
+
+const Welcome = () =>{
+    return(
+        <div className='grid grid-main justify-center w-full h-full items-center'>
+            <div className='col-span-8'>
+                <img src={require('../assets/logo.png')} className='h-24 mx-auto w-24'/>
+                <div className='my-5 text-5xl'>Welcome to PennThrift!</div>
+                <div className='w-full h-[1px] bg-[gray]'></div>
+                <div className='my-5'>Your one-stop-shop for buying, trading, gifting, and thrifting at Penn.</div>
+                <div className='flex mx-16 justify-between'>
+                    <div className='flex flex-col text-center'>
+                        <div className=''>
+                            New Here?
+                        </div>
+                        <Link to='/register' className='bg-[#368481] my-3 w-28 rounded-[30px] h-12 text-white flex justify-center items-center'>
+                            Register
+                        </Link>
+                    </div>
+                    <div className='flex flex-col text-center'>
+                        <div className=''>
+                            Returning User?
+                        </div>
+                        <Link to='/login'  className='bg-[#368481] my-3 w-28 rounded-[30px] h-12 text-white flex justify-center items-center'>
+                            Login
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        
+    )
+}
+
+
+
+export default Welcome;
