247a41f8436871a05da02e9b4faf233b02d611a3
mloirraqi <mloirraqi@gmail.com>
2022-04-21 13:52:36 -0400

    Update schema and .env

--- server/models/AuthSchema.js
+++ server/models/AuthSchema.js
@@ -3,32 +3,39 @@ const { Schema } = mongoose;
 const ObjectId = Schema.ObjectId;
 
 const User = new Schema({
-    id: ObjectId,
-    username: {
-        type: String,
-        required: true,
-        minLength: 1,
-        maxLength: 25,
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 1,
-        maxLength: 60,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    venmo: {
-        type: String,
-        minLength: 5,
-        maxLength: 16,
-    },
-    date:{
-        type: Date,
-        default:Date.now,
-    },
+  id: ObjectId,
+  username: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 25,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 60,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  venmo: {
+    type: String,
+    minLength: 5,
+    maxLength: 16,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  items: [{ type: Schema.Types.ObjectId, ref: 'Item' }],
+  reviews_for: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
+  reviews_to: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
+  requests_for: [{ type: Schema.Types.ObjectId, ref: 'Request' }],
+  requests_to: [{ type: Schema.Types.ObjectId, ref: 'Request' }],
+  chats: [{ type: Schema.Types.ObjectId, ref: 'Chat' }],
+  pending_notifs: [{ type: Schema.Types.ObjectId, ref: 'Notification' }],
 });
 
-module.exports = mongoose.model('userTable', User);
+module.exports = mongoose.model('PennThriftBackend', User, 'User');
