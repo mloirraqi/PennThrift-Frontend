9ef616c7f1721f29a66b3e7121c0dadf191df310
mloirraqi <mloirraqi@gmail.com>
2022-04-23 21:49:47 -0400

    Add edit description and update profile pic

--- client/src/pages/EditProfile.js
+++ client/src/pages/EditProfile.js
@@ -1,10 +1,10 @@
 import Header from "../components/Header"
-
+import { useState } from "react";
 
 
 
 const EditProfile = props => {
-
+    const [description, setDescription] = useState('Edit description')
 
     return(
         <div>
@@ -16,7 +16,12 @@ const EditProfile = props => {
                         <img
                             className="w-60 h-60" 
                             src={require('../assets/placeholder_user.png')}/>
-
+                        <div className="text-blue-400 underline cursor-pointer my-2">Upload a profile photo</div>
+                        <textarea 
+                            onChange={event => setDescription(event.target.value)} 
+                            value={description} 
+                            style={{resize:"none"}}
+                            className="w-full border  h-20 p-5"/>
                     </div>
                     <div className="xl:col-span-3 col-span-5 h-fit grid gap-10 ">
                         <div className="">
@@ -87,6 +92,9 @@ const EditProfile = props => {
                         </div>
                         <div className="">
                             <div className="font-semibold">My venmo is: <input className="bg-[#F8F8F8] ml-2 outline-0 border border-black px-2"/></div>
+                        </div>
+                        <div>
+                            <div className="flex border-2 rounded w-fit px-5 cursor-pointer border-black flex">Save</div>
                         </div>
                     </div>
                 </div>
