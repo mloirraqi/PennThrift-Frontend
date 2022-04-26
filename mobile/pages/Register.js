e969ed3e74d106a9e717b9786e01d4bcffff9bfe
Christian-Villegas <crv2700@gmail.com>
2022-04-26 17:48:47 -0400

    form takes input now and finally on on reg. page

--- mobile/pages/Register.js
+++ mobile/pages/Register.js
@@ -1,16 +1,49 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
-// <View style={styles.container}>
-    //   <Text>Hello Julia</Text>
-    //   <StatusBar  style="auto" />
-    // </View>
+import Form from '../components/Form';
 
 const Register = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <Text>Hello Julia</Text>
-            <StatusBar  style="auto" />
+    const [error, setError] = React.useState();
+    const address = 'http://localhost:4000/api/register'; 
+
+    function userDetails(username,password){
+        const data = {
+            'username':username,
+            'password':password,
+            'email':username,
+        };
+
+
+        // axios.post(address, data).then(res =>{
+        //     if(res.data === "error"){
+        //         setError('Username has already been taken');
+        //     }else{
+        //         localStorage.setItem('username',username)
+        //         navigate('/profile', { replace: true })
+        //     }
+            
+        // })
+        
+    }
+
+    function reset(){
+        setError(null)
+    }
+
+    return(
+        <View className='grid grid-main justify-center w-full h-full items-center'>
+            <View className='col-span-8 flex flex-col justify-center'>
+                <View className='my-5 text-center text-5xl'><Text>Welcome!</Text></View>
+                <View className='w-full my-10 h-[1px] bg-[gray]'></View>
+                <Form
+                userDetails={userDetails}
+                reset={reset}
+                error={error}
+                name='Register'/>
+            </View>
         </View>
+        
     )
 };
 
