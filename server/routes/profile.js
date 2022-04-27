a9ba8103aaec6b30feb67f1a77d88e744b5f3f98
Alex Scott <axs.scott@gmail.com>
2022-04-26 21:45:20 -0400

    profile.js uses username instead of id

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -9,25 +9,32 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error! ' + err))
 });
 
-// get profile/ user info by id
-router.route('/:id').get((req, res) => {
-    User.findById(req.params.id)
+// get profile/ user info by username
+router.route('/:username').get((req, res) => {
+    User.findOne({username: req.params.username})
     .then(user => res.json(user))
     .catch(err => res.status(400).json('Error! ' + err))
 });
 
 // delete profile/user by id
-router.route('/delete/:id').delete((req, res) => {
-    User.deleteOne({ _id: req.params.id })
+router.route('/delete/:username').delete((req, res) => {
+    User.deleteOne({ username: req.params.username })
         .then(success => res.json('Success! User deleted.'))
         .catch(err => res.status(400).json('Error! ' + err))
 });
 
 // edit profile/user info by id
-router.route('/edit/:id').put((req, res) => {
-    User.findByIdAndUpdate(req.params.id, req.body)
+router.route('/edit/:username').put((req, res) => {
+    User.findOneAndUpdate({username: req.params.username }, req.body)
         .then(user => res.json('Success! User updated.'))
         .catch(err => res.status(400).json('Error! ' + err))
+});
+
+// get items of user
+router.route('/items/:username').get((req, res) => {
+    User.findOne({ username: req.params.username }, {username: 1, items: 1})
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error! ' + err))
 });
 
 module.exports = router;
