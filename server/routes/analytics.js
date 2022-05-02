e9e9d31d78c72549edece684ad84a6e428dc9722
Alex Scott <axs.scott@gmail.com>
2022-05-02 19:56:35 -0400

    analytic update + updated routes

--- server/routes/analytics.js
+++ server/routes/analytics.js
@@ -2,13 +2,13 @@ const User = require('../models/user.model');
 const Item = require('../models/item.model')
 const router = require('express').Router();
 
-router.route('/views/:username').get((req, res) => {
+router.route('/profile/views/:username').get((req, res) => {
     User.findOne({username: req.params.username}, {profile_views: 1})
     .then(user => res.json(user))
     .catch(err => res.status(400).json('Error! ' + err))
 });
 
-router.route('/views/update/:username').get((req, res) => {
+router.route('/profile/update/:username').put((req, res) => {
     User.findOneAndUpdate({username: req.params.username}, req.body)
     .then(user => res.json('Success! User analytics updated.'))
     .catch(err => res.status(400).json('Error! ' + err))
