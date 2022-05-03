579074c376b691eb1b358116a8eeec2a9fdd7164
Alex Scott <axs.scott@gmail.com>
2022-05-02 20:11:08 -0400

    added functionality to get item total views

--- server/routes/analytics.js
+++ server/routes/analytics.js
@@ -12,6 +12,12 @@ router.route('/profile/update/:username').put((req, res) => {
     User.findOneAndUpdate({username: req.params.username}, req.body)
     .then(user => res.json('Success! User analytics updated.'))
     .catch(err => res.status(400).json('Error! ' + err))
-})
+});
+
+router.route('/item/views/:id').get((req, res) => {
+    Item.findById(req.params.id, {views: 1})
+    .then(item => res.status(200).json(item))
+    .catch(err => res.status(400).json('Error! ' + err))
+});
 
 module.exports = router;
